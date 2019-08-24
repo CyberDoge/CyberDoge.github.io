07567eedc60c99348657e5aed47bc8a1aabe952c
Tagir <tagirman998@gmail.com>
2019-08-24 15:35:35 +0400

    подправил верстку, добавил окно информации

--- js/login.js
+++ js/login.js
@@ -1,7 +1,16 @@
+const userInfo = `
+    <div class="user-info central-block">
+        <img class="user-icon" alt="user icon"/>
+        <div class="user-name"></div>
+        <button class="btn-logout active-btn" onclick="logout()">Logout</input>
+    </div>
+`;
+
+
 const emailInput = document.querySelector(".form__input_email");
 const passwordInput = document.querySelector(".form__input_password");
-
-let parseForm = () => {
+const page = document.querySelector(".page");
+const parseForm = () => {
     return JSON.stringify({
         "email": emailInput.value,
         "password": passwordInput.value
@@ -9,7 +18,12 @@ let parseForm = () => {
 };
 
 const form = document.querySelector(".form");
+
+let wasSent = false;
+
 form.onsubmit = (e) => {
+    if (wasSent) return false;
+    wasSent = true;
     e.preventDefault();
     const request = new XMLHttpRequest();
     request.open("POST", "https://us-central1-mercdev-academy.cloudfunctions.net/login");
@@ -23,17 +37,30 @@ form.onsubmit = (e) => {
                 const invalidEmail = document.querySelector(".form__invalid-email");
                 invalidEmail.classList.add("hide");
                 invalidEmail.classList.remove("last");
-                console.log(request.responseText)
+                setUserInfo(JSON.parse(request.responseText));
             } else if (request.status === 400) {
                 emailInput.classList.add("form__input_email-wrong");
                 passwordInput.classList.remove("last");
                 const invalidEmail = document.querySelector(".form__invalid-email");
                 invalidEmail.classList.remove("hide");
                 invalidEmail.classList.add("last");
-                console.log(request.responseText)
             }
+            wasSent = false;
         }
     };
 
     request.send(parseForm());
+};
+
+const setUserInfo = (jsonObj) => {
+    form.classList.add("hide");
+    page.insertAdjacentHTML("beforeend", userInfo);
+    const userInfoDiv = document.querySelector(".user-info");
+    userInfoDiv.querySelector(".user-icon").src = jsonObj.photoUrl;
+    userInfoDiv.querySelector(".user-name").textContent = jsonObj.name;
+};
+
+const logout = () => {
+    page.removeChild(document.querySelector(".user-info"));
+    form.classList.remove("hide");
 };
