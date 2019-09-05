1f6beb6d9fa18596adc75297b6b91b1c9e040543
Tagir <tagirman998@gmail.com>
2019-09-05 19:29:43 +0400

    исправил webstorm оформеление кода на prettier (тоже тз)

--- js/auth-model-validation-service.js
+++ js/auth-model-validation-service.js
@@ -1,17 +1,19 @@
 export default class AuthModelValidationService {
-
-
     /**
      *
      * @param {AuthModel} authModel
      * @returns {string} Error message
      */
     static validate(authModel) {
-        if (authModel.email === '' || !authModel.email.trim() || authModel.password === '') {
-            return 'Email or password is blank or empty';
+        if (
+            authModel.email === '' ||
+            !authModel.email.trim() ||
+            authModel.password === ''
+        ) {
+            return 'Email or password is blank or empty'
         }
         if (!/\S+@\S+\.\S+/.test(authModel.email)) {
-            return 'Invalid email format';
+            return 'Invalid email format'
         }
     }
 }
--- js/page-content-controller.js
+++ js/page-content-controller.js
@@ -1,48 +1,48 @@
 export default class PageContentController {
     constructor() {
-        this.emailInput = document.querySelector(".form__input_email");
-        this.passwordInput = document.querySelector(".form__input_password");
-        this.invalidEmail = document.querySelector(".form__invalid-email");
-        this.centralBlock = document.querySelector(".central-block");
-        this.form = document.querySelector(".form");
-        this.userInfoTemplate = document.querySelector("#user-info-template");
+        this.emailInput = document.querySelector('.form__input_email');
+        this.passwordInput = document.querySelector('.form__input_password');
+        this.invalidEmail = document.querySelector('.form__invalid-email');
+        this.centralBlock = document.querySelector('.central-block');
+        this.form = document.querySelector('.form');
+        this.userInfoTemplate = document.querySelector('#user-info-template')
     }
 
     setFormOnSubmit(onsubmit) {
-        this.form.onsubmit = onsubmit;
+        this.form.onsubmit = onsubmit
     }
 
     showWrongEmailErrorDiv(message) {
-        this.emailInput.classList.add("form__input_email-wrong");
-        this.passwordInput.classList.remove("last");
-        this.invalidEmail.classList.remove("hide");
-        this.invalidEmail.classList.add("last");
-        if (message) this.invalidEmail.innerHTML = message;
+        this.emailInput.classList.add('form__input_email-wrong');
+        this.passwordInput.classList.remove('last');
+        this.invalidEmail.classList.remove('hide');
+        this.invalidEmail.classList.add('last');
+        if (message) this.invalidEmail.innerHTML = message
     }
 
     setUserInfo(jsonObj) {
         this.centralBlock.removeChild(this.form);
-        this.centralBlock.appendChild(document.importNode(this.userInfoTemplate.content, true));
-        const userInfoDiv = document.querySelector(".user-info");
-        userInfoDiv.querySelector(".user-icon").src = jsonObj.photoUrl;
-        userInfoDiv.querySelector(".user-name").textContent = jsonObj.name;
-    };
+        this.centralBlock.appendChild(
+            document.importNode(this.userInfoTemplate.content, true)
+        );
+        const userInfoDiv = document.querySelector('.user-info');
+        userInfoDiv.querySelector('.user-icon').src = jsonObj.photoUrl;
+        userInfoDiv.querySelector('.user-name').textContent = jsonObj.name
+    }
 
     hideUserInfoAndShowForm() {
-        this.centralBlock.removeChild(document.querySelector(".user-info"));
+        this.centralBlock.removeChild(document.querySelector('.user-info'));
         this.centralBlock.appendChild(this.form)
-    };
-
+    }
 
     hideWrongEmailErrorDiv() {
-        this.emailInput.classList.remove("form__input_email-wrong");
-        this.passwordInput.classList.add("last");
-        this.invalidEmail.classList.add("hide");
-        this.invalidEmail.classList.remove("last");
+        this.emailInput.classList.remove('form__input_email-wrong');
+        this.passwordInput.classList.add('last');
+        this.invalidEmail.classList.add('hide');
+        this.invalidEmail.classList.remove('last')
     }
 
     setLogoutOnClick(onclick) {
-        document.querySelector(".btn-logout").onclick = onclick;
+        document.querySelector('.btn-logout').onclick = onclick
     }
-
 }
